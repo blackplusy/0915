a593c65b8ba770b74a5fca93b0340d2c5318177d
blackplusy <osimeno@163.com>
2020-09-28 11:41:19 +0800

    Add files via upload

---
+++ 例子-0928-03.运算符.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+#1.算数
+print(10+3)
+
+print(10-3)
+
+print(10/3)
+
+print(10*3)
+
+print(10%3)
+
+print(11//3)
+
+#2.比较
+print(1>2)
+
+print(2>=2)
+
+print('A'>'a')
+
+print('王'>'张')
+
+#3.逻辑
+print(1>2 and 2>1)
+
+print(1>2 or 2>1)
+
+print(not(1>2))
+
