77680e85ee6d7659f47e59c91cafce41c6c15522
blackplusy <osimeno@163.com>
2020-09-28 14:37:41 +0800

    Add files via upload

---
+++ 例子-0928-04.成员运算符.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+#字符串
+name='hello world!'
+
+#循环
+
+for i in name:
+    print(i)
+
+#列表
+
+l=['kobe','rose','yao']
+
+for i in l:
+    print(i)
+
+#判断
+
+if 'e' in name:
+    print('e is here！')
---
+++ 例子-0928-05.条件分支判断.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+#单分支
+'''
+name=input('请输入您大表哥的名字： ')
+if name=='蔡徐坤':
+    print('及你太美！')
+'''
+#双分支
+'''
+name=input('为什么叫我贝塔？')
+if name=='巧碧螺':
+    print('its me！！！')
+else:
+    print('舒克的飞机刷没了！！')
+'''
+
+#多分支
+name=input('请输入您喜欢的主播: ')
+if name=='黑哥gaga':
+    print('来，刷火箭！！！！')
+elif name=='55开':
+    print('难受啊马飞！')
+elif name=='大司马':
+    print('正方形打野！')
+else:
+    print('来，刷波666')
